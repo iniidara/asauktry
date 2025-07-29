a6f1d25fa4664b9558ce819dce1d8bef60ea96df
iniidara <iniidaraisaiah@gmail.com>
2025-07-29 13:01:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showSidebar(){
+    document.getElementById('sidebar').style.display = 'flex';
+}
+function hideSidebar(){
+    document.getElementById('sidebar').style.display = 'none';
+}
